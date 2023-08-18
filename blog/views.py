d9bd6d9993734f52cc569a34765f5784b74d23ba
Jacob Barnette <jacobcbarnette@gmail.com>
2023-08-18 14:37:39 -0400

    removed dummy data from models.py

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render
 from .models import Post
-posts = [
-        {
-            'author': 'Jacob B',
-            'title': 'My First Blog Post',
-            'content': 'First post content',
-            'date_posted': 'August 27, 2018'
-        },
-             {
-            'author': 'Jacob B',
-            'title': 'My Second Blog Post',
-            'content': 'Second  post content',
-            'date_posted': 'August 27, 2018'
-        }
-        ]
 
 
 def home(request):
