8d89d2b3dbafe7dcc6b44453f4de617426fc4b03
Jacob Barnette <jacobcbarnette@gmail.com>
2023-08-21 09:57:41 -0400

    added crispy forms and ability to save user to db

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -33,6 +33,7 @@ CORS_ALLOWED_ORIGINS = [
 INSTALLED_APPS = [
     'users.apps.UsersConfig',    
     'blog.apps.BlogConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
