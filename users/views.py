3c136b13972527c687ea6df29f03d4b758147a49
Jacob Barnette <jacobcbarnette@gmail.com>
2023-08-21 12:58:48 -0400

    updated routes in navbar for when user is login/logout

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username}!')
+            messages.success(request, f'Your account has been created! You are now able to login')
             return redirect('blog-home')
     else:
         form = UserRegisterForm()
